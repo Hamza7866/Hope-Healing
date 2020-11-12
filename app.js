8eac2d5058895e623caa5f36176a278e0ce3b409
Hamza Muhammad <50344116+Hamza7866@users.noreply.github.com>
2020-11-12 01:50:08 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const ham = document.getElementById('ham');
+const navBar = document.getElementById('navbar')
+const youtube = document.getElementById('youtube')
+
+ham.addEventListener('click',()=>{
+    navBar.classList.toggle('navbar-troggle');
+   
+})
